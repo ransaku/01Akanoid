73dacde8326cdb5d3d6dcf158926e371d54d1915
ransaku <huyrt1994@gmail.com>
2019-04-25 03:29:14 +0800

    FinalVersion
    这个练习做完比较久了，应该不会再修改了。

---
+++ Assets/Scripts/CameraMotion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion : MonoBehaviour {
+
+    public float cameraspeed = 4;
+    //设置可控属性组件来控制镜头初始移动速度
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        float h = Input.GetAxis("Horizontal");
+        //GetAxis获取轴状态（Horizontal为水平轴，即x方向坐标），返回值h会根据按下的方向键左右有-1到0到1的变化
+        transform.Translate(new Vector3(h, 0, 0) * Time.deltaTime * cameraspeed);
+        //利用transform中的Translate组件获取方向
+        //不做按键操作时h为0，方向不改变。按→时h返回值为1则向(1,0,0)移动，即向右移动，按←时返回值为-1向左移动。
+        //由于移动时h的值为1，1帧运行1米，1s约有60帧，按一下1s时间将移动60米。速度太快，需要乘deltaTime(约60分之1)来控制时间间隔，当前速度将变为1s移动1米。
+        //再增加cameraspeed变量使镜头速度可控
+
+        float v = Input.GetAxis("Vertical");
+        //Vertical为垂直轴，即y方向坐标。
+        transform.Translate(new Vector3(0, v, 0) * Time.deltaTime * cameraspeed);
+        //控制镜头纵向移动
+
+        
+        /*上述代码可合并书写*/
+        /*
+         float h = Input.GetAxis("Horizontal");
+         float v = Input.GetAxis("Vertical");
+         
+         transform.Translate(new Vector3(h, v, 0) * Time.deltaTime * cameraspeed);
+
+         */
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    //创建子弹GameObject（需手动将子弹的prefab挂载到该GameObject上）
+    public float speed = 25;
+    //设置子弹初速度为25
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("hello unity");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //检测鼠标左键点击，0为左键
+            GameObject b = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            //以当前对象（脚本挂载在camera上因此当前对象为camera）坐标和角度实例化子弹bullet，
+            //并将返回值赋值给创建的实例对象b
+            Rigidbody rgb = b.GetComponent<Rigidbody>();
+            //获取实例对象b身上的刚体组件Rigidbody
+            rgb.velocity = transform.forward * speed;
+            //给该实例对象的刚体组件中的速度赋值，transform.forward为z轴正方向，
+            //speed为初速度（speed值可在inspector面板中进行更改，并且以面板中赋值优先）
+        }
+
+    }
+}
